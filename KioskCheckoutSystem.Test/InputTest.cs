c48afcd34f810b2d072fdf7af45f26c385ea5a5b
R P Iliuta <patricia.iliuta@gmail.com>
2017-01-30 11:54:36 -0700

    Added a test project

---
+++ KioskCheckoutSystem.Test/InputTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KioskCheckoutSystem.Test
+{
+    [TestClass]
+    public class InputTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

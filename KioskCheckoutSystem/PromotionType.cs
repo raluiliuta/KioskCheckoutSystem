276459edb8b19d1e9e764bf3aa63a27850cfee0c
R P Iliuta <patricia.iliuta@gmail.com>
2017-01-29 22:08:08 -0700

    Added promotion types and updated Promotion.json
    Now every Promotion object should be attributed a PromotionType.
    Depending on the PromotionType the priceDiscountInformation has different semantics:
    	[FixedDiscount]  - the new price of all the products specified in condition
    	[PercentDiscount] -  the percentage of the regular price that constitutes the new price

---
+++ KioskCheckoutSystem/PromotionType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioskCheckoutSystem
+{
+    /// <summary>
+    /// Type of promotion
+    /// </summary>
+    public enum PromotionType
+    {
+        /// <summary>
+        /// Defines a new price for a quantity 
+        /// </summary>
+        /// <remarks>
+        /// Ex: 2 Apples at 3$
+        /// Ex: 1 Orange at 1$
+        /// </remarks>
+        FixedDiscount,
+        /// <summary>
+        /// Defines a percentage reduction of the regular price
+        /// </summary>
+        ///<remarks>
+        ///Ex: Buy 1 get 1 free is actually a 50% reduction if buying 2 items
+        ///</remarks> 
+        PercentDiscount       
+    }
+}
